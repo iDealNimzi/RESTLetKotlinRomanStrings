c8207b2ddaf780996c018dc92da22248191e4917
iDealNimzi <eric.manzitti@gmail.com>
2023-10-30 08:33:54 -0400

    MainTest to only instantiate restlet once for upcoming tests

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -2,11 +2,19 @@
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.junit.Assert.assertEquals
+import org.junit.BeforeClass
 import org.junit.Test
 
 class MainTest {
     //declare HTTPClient used for testing
     private val client : OkHttpClient
+    //Initialize the Restlet Once for all tests
+    companion object {
+        @BeforeClass
+        @JvmStatic fun setup() {
+            main()
+        }
+    }
     //Initalize builder and client
     init {
         val newBuilder = OkHttpClient.Builder()
@@ -15,7 +23,7 @@ class MainTest {
     @Test
     fun `test launching of Restlet localhost`() {
 
-        main()
+
         //port 7070 hard coded for localhost
         val response = client.newCall(Request.Builder().url("http://localhost:7070/").build()).execute()
         assertEquals(200, response.code)
